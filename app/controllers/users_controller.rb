9293dd1952866d4f707b1739cc1f33c0919922a5
Andrei Duma <andrei.duma.dorian@gmail.com>
2013-12-15 14:19:27 +0200

    Changed stuff

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -57,7 +57,7 @@ class UsersController < ApplicationController
 		@user = User.find(session[:user_id])
 		hash = SecureRandom.hex(8)
 		@user.events.create({ :event_hash => hash, :status => "created", :start_date => Time.now, :end_date => nil, :money_limit => 0, :host_id => @user.id, :has_started => false})
-		redirect_to "/event/" + hash + "/edit"
+		redirect_to "/event/" + hash
 #		session[:user_id] suuper! :)	
 	end
 
